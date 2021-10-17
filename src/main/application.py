8c738cb19d0305f4e3485a1ef6670d1628532019
brkkaya <brk.kya34@yahoo.com>
2021-10-17 18:49:06 +0300

    Add path provider

--- src/main/application.py
+++ src/main/application.py
@@ -1,9 +1,12 @@
+from src.services.Path_provider import GLobalPathProvider
 from src.services.logger import Logger
 
 
 class Application(Logger):
     def __init__(self):
         self.log.info("Hello world")
+        path_provider = GLobalPathProvider(file_name="hello")
+        self.log.info((path_provider.path))
 
 
 if __name__ == "__main__":
