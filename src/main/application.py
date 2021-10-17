f282abcb79f4eedac14b081c47dd5672a5ea8b96
brkkaya <brk.kya34@yahoo.com>
2021-10-17 17:26:26 +0300

    Init to YamlLoader

--- src/main/application.py
+++ src/main/application.py
@@ -1,9 +1,11 @@
 from src.services.logger import Logger
+from src.services.yaml_loader import YamlLoader
 
 
 class Application(Logger):
     def __init__(self):
         self.log.info("Hello world")
+        YamlLoader()
 
 
 if __name__ == "__main__":
