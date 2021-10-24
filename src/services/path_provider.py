c228a1d0371e23996fa2f2054d46bed16eb5c374
brkkaya <brk.kya34@yahoo.com>
2021-10-24 13:05:15 +0300

    Add path provider

---
+++ src/services/path_provider.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+from typing import Union
+from datetime import datetime
+
+
+class GLobalPathProvider:
+    def __init__(self, file_name: Union[str, None]) -> None:
+        if file_name:
+            f_name = file_name + ".log"
+        else:
+            f_name = "console.log"
+        __date = datetime.now().strftime("%m-%Y/%d/%H:%M:%S")
+        self.path = str(Path(__file__).parent.parent.parent)
+        self.log_path = self.path + "/logs/" + __date
+        os.makedirs(self.log_path, exist_ok=True)
+        self.log_path += "/" + f_name
